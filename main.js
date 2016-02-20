62b71bf0070b8cc1a95270732ae4480bf53336c7
looseleaf0727 <2nas2nik@gmail.com>
2016-02-20 15:53:09 +0900

    追加

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
-    parser = require('body-parser');
-var knex = require('knex')({ //knexの使用
+    parser = require('body-parser'),
+	knex = require('knex')({ //knexの使用
   		client: 'sqlite3',
   		connection: {
     		filename: ":memory:"
@@ -34,18 +34,36 @@ var knex = require('knex')({ //knexの使用
 
     app.get('/api/ping', function (req, res, next) {
     	res.json('PONG');
-	return next();
+		return next();
     });
 
     app.get('/api/notfound', function (req, res, next) {
     	res.status(404).json('NotFound');
-	return next();
+		return next();
     });
 
     app.post('/api/badrequest', function (req, res, next) {
-    	res.status(400).json('BadRequest');
-	return next();
-    });
+		var title = req.body.title,//postの場合はbodyに入る
+		description = req.body.description,
+		url = req.body.url;
+		
+		knex('projects').inserts({//projectsに対して
+			title:title,
+			description:description,
+			url:url
+		}).then(function (ids){
+			res.json({
+				id: ids[0],
+				title: title,
+				description: description,
+				url: url
+			})
+			return next();
+			}).catch(function (err){
+				res.status(500).json(err);
+				return next();
+			});
+		});
 
 /*
     app.listen(port, function () {
